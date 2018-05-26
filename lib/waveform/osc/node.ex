c11662e6febc03f82d9fe2e7c13dc0b8fe357eb7
Ryan Messner <ryan@hashrocket.com>
2018-05-26 18:06:09 -0500

    add play command to iex

---
+++ lib/waveform/osc/node.ex
@@ -0,0 +1,30 @@
+defmodule Waveform.OSC.Node do
+  defmodule ID do
+    use GenServer
+
+    @me __MODULE__
+
+    defmodule State do
+      defstruct(
+        current_id: 0
+      )
+    end
+
+    def next_id do
+      GenServer.call(@me, {:next})
+    end
+
+    def start_link(initial_id \\ 0) do
+      GenServer.start_link(@me, %State{current_id: initial_id}, name: @me)
+    end
+
+    def init(state) do
+      {:ok, state}
+    end
+
+    def handle_call({:next}, _from, state) do
+      state = %{state | current_id: state.current_id + 1}
+      {:reply, state.current_id, state}
+    end
+  end
+end
