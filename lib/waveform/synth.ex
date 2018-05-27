42dac8520531ad8880034147a02b225fe6531646
Ryan Messner <ryan@hashrocket.com>
2018-05-26 21:36:03 -0500

    Allow duration argument to synth

--- lib/waveform/synth.ex
+++ lib/waveform/synth.ex
@@ -3,20 +3,28 @@ defmodule Waveform.Synth do
   alias Waveform.OSC, as: OSC
   alias Waveform.OSC.Node.ID, as: ID
 
+  import Waveform.Util
+
   @params_whitelist [
-    :pan, :release, :attack
+    :amp, :pan, :release, :attack, :sustain
   ]
   @s_new 's_new'
   @default_synth 'sonic-pi-prophet'
 
-  def play(note) when is_atom(note), do: (note |> Music.to_midi |> play)
-  def play(note) when is_number(note), do: play([:note, note])
-  def play(note, args) when is_atom(note), do: play(Music.to_midi(note), args)
-  def play(note, args) when is_list(args), do: play(note, Enum.into(args, %{}))
-  def play(note, args) when is_number(note) and is_map(args) do
-    play(Enum.reduce(args, [:note, note], whitelister))
+  def play(note), do: synth(note)
+  def play(note, args), do: synth(note, args)
+
+  def synth(note) when is_atom(note), do: (note |> Music.to_midi |> play)
+  def synth(note) when is_number(note), do: play([:note, note])
+  def synth(note, args) when is_atom(note), do: play(Music.to_midi(note), args)
+  def synth(note, args) when is_list(args), do: play(note, Enum.into(args, %{}))
+  def synth(note, args) when is_number(note) and is_map(args) do
+    args
+    |> calculate_sustain
+    |> Enum.reduce([:note, note], normalizer)
+    |> synth
   end
-  def play(args) when is_list(args) do
+  def synth(args) when is_list(args) do
     node_id = ID.next_id
     group_id = 0
     synth_name = @default_synth
@@ -27,8 +35,8 @@ defmodule Waveform.Synth do
     OSC.send_command([@s_new, synth_name, node_id, add_action, group_id | args])
   end
 
-  defp whitelister, do: fn {key, value}, coll ->
-    if Enum.member? @params_whitelist, key do
+  defp normalizer, do: fn {key, value}, coll ->
+    if Enum.member?(@params_whitelist, key) && is_number(value) do
       [key, value | coll]
     else
       coll
---
+++ lib/waveform/util.ex
@@ -0,0 +1,17 @@
+defmodule Waveform.Util do
+  def calculate_sustain(args) do
+    if Map.has_key?(args, :duration) and !(Map.has_key?(args, :sustain)) do
+      attack = args[:attack] || 0
+      decay = args[:decay] || 0
+      release = args[:release] || 0
+      duration = args[:duration]
+      sustain = duration - (attack + decay + release)
+
+      args
+      |> Map.delete(:duration)
+      |> Map.merge(%{sustain: Enum.max([0, sustain])})
+    else
+      args
+    end
+  end
+end
