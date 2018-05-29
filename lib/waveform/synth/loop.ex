ccc427003aacacb6df7b2aa54863a719beba16c1
Ryan Messner <ryan@hashrocket.com>
2018-05-28 21:08:36 -0500

    include loop macro

---
+++ lib/waveform/synth/loop.ex
@@ -0,0 +1,14 @@
+defmodule Waveform.Synth.Loop do
+  defmacro repeat(name, do: body) do
+  end
+
+  defmacro loop(do: body) do
+    IO.puts("hi")
+  end
+
+  def kill(name) do
+  end
+
+  def kill_all(name) do
+  end
+end
